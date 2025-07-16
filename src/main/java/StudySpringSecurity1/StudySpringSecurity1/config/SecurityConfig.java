ac1fe30c00aa4285af1daec0256ff6385c621d62
mdy3722 <mdy3722@gmail.com>
2025-07-16 10:38:12 +0900

    feat: Spring Security Config 설정

---
+++ src/main/java/StudySpringSecurity1/StudySpringSecurity1/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package StudySpringSecurity1.StudySpringSecurity1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+    @Bean  // SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/**").authenticated()   // user라는 url로 들어오면 인증이 필요하다.
+                        .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")  // manager으로 들어오는 MANAGER 인증 또는 ADMIN인증이 필요하다
+                        .requestMatchers("/admin/**").hasRole("ADMIN")  // admin으로 들어오면 ADMIN 권한이 있는 사람만 들어올 수 있다.
+                        .anyRequest().permitAll()    // 그리고 나머지 url은 전부 권한을 허용해준다.
+                );
+
+        http.formLogin(form -> form     // 인증 필요 시 /login 페이지로 리다이렉트
+                .loginPage("/login"));
+        return http.build();
+    }
+
+}
--- src/main/java/StudySpringSecurity1/StudySpringSecurity1/controller/IndexController.java
+++ src/main/java/StudySpringSecurity1/StudySpringSecurity1/controller/IndexController.java
@@ -2,6 +2,7 @@ package StudySpringSecurity1.StudySpringSecurity1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller   // view를 리턴
 public class IndexController {
@@ -10,5 +11,36 @@ public class IndexController {
         // 머스테치 기본폴더 src/main/resources/
         // 뷰리졸버 설정 : templates (prefix), .mustache (suffix)
         return "index";   // 얘는 결과적으로 src/main/resources/templates/index.mustache를 찾는다. => 우리가 생성한 것은 index.html 파일인데 이는 WebMvcConfig.java에서 설정한다.
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 시큐리티 설정이 별도로 없으면 이 매핑은 스프링시큐리티 해당주소로 낚아채짐
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public @ResponseBody String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/signupProc")
+    public @ResponseBody String signupProc() {
+        return "회원가입 완료";
     }
 }
